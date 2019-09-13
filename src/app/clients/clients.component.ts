124fbf0411f85621ed9f810a84998525724d8f53
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2019-09-12 19:36:48 -0600

    Lectura 16: Creando archivo clientes.json.ts con la lista de objetos

--- src/app/clients/clients.component.ts
+++ src/app/clients/clients.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Client } from './client';
+import { CLIENTS } from './clients.json';
 
 @Component({
   selector: 'app-clients',
@@ -8,47 +9,12 @@ import { Client } from './client';
 })
 export class ClientsComponent implements OnInit {
 
-  public clients: Client[] = [
-    {
-      id: 1,
-      firstName: 'Alejandro',
-      lastName: 'Rodarte',
-      createdAt: '2019-09-12',
-      email: 'alex@gmail.com'
-    },
-    {
-      id: 2,
-      firstName: 'Patricia',
-      lastName: 'Mendoza',
-      createdAt: '2019-09-11',
-      email: 'paty@gmail.com'
-    },
-    {
-      id: 3,
-      firstName: 'Magdaleno',
-      lastName: 'Rodarte',
-      createdAt: '2019-09-10',
-      email: 'max@gmail.com'
-    },
-    {
-      id: 4,
-      firstName: 'Pepe',
-      lastName: 'Rodarte',
-      createdAt: '2019-09-09',
-      email: 'pepe@gmail.com'
-    },
-    {
-      id: 5,
-      firstName: 'Lola',
-      lastName: 'Mendoza',
-      createdAt: '2019-09-08',
-      email: 'lola@gmail.com'
-    }
-  ];
+  public clients: Client[];
 
   constructor() { }
 
   ngOnInit() {
+    this.clients = CLIENTS;
   }
 
 }
---
+++ src/app/clients/clients.json.ts
@@ -0,0 +1,37 @@
+export const CLIENTS = [
+  {
+    id: 1,
+    firstName: 'Alejandro',
+    lastName: 'Rodarte',
+    createdAt: '2019-09-12',
+    email: 'alex@gmail.com'
+  },
+  {
+    id: 2,
+    firstName: 'Patricia',
+    lastName: 'Mendoza',
+    createdAt: '2019-09-11',
+    email: 'paty@gmail.com'
+  },
+  {
+    id: 3,
+    firstName: 'Magdaleno',
+    lastName: 'Rodarte',
+    createdAt: '2019-09-10',
+    email: 'max@gmail.com'
+  },
+  {
+    id: 4,
+    firstName: 'Pepe',
+    lastName: 'Rodarte',
+    createdAt: '2019-09-09',
+    email: 'pepe@gmail.com'
+  },
+  {
+    id: 5,
+    firstName: 'Lola',
+    lastName: 'Mendoza',
+    createdAt: '2019-09-08',
+    email: 'lola@gmail.com'
+  }
+];
