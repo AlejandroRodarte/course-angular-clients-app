6bb4deceaee05452bafe5ce127435cafb4b2e27e
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2019-09-12 19:54:12 -0600

    Lectura 18: Implementando Observable en nuestra clase Servicio ClienteService

--- src/app/clients/client.service.ts
+++ src/app/clients/client.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
 import { CLIENTS } from './clients.json';
 import { Client } from './client.js';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClientService {
+
+  public clientsUpdated: Subject<Client[]> = new Subject<Client[]>();
 
   constructor() {
 
--- src/app/clients/clients.component.ts
+++ src/app/clients/clients.component.ts
@@ -1,20 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Client } from './client';
 import { ClientService } from './client.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-clients',
   templateUrl: './clients.component.html',
   styleUrls: ['./clients.component.css']
 })
-export class ClientsComponent implements OnInit {
+export class ClientsComponent implements OnInit, OnDestroy {
 
   public clients: Client[];
+
+  public clientSubscription: Subscription;
 
   constructor(private clientService: ClientService) { }
 
   ngOnInit() {
     this.clients = this.clientService.getClients();
+    this.clientSubscription = this.clientService.clientsUpdated.subscribe(clients => this.clients = clients);
+  }
+
+  ngOnDestroy() {
+    this.clientSubscription.unsubscribe();
   }
 
 }
