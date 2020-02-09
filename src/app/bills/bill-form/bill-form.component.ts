38e5fa4a2d8db758c5088ce605345923fe596097
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-02-08 20:40:04 -0700

    FormArray of bill items working with autocomplete features

--- src/app/bills/bill-form/bill-form.component.ts
+++ src/app/bills/bill-form/bill-form.component.ts
@@ -3,12 +3,13 @@ import * as fromApp from '../../store/app.reducer';
 import * as BillActions from '../store/bills.actions';
 import { Store } from '@ngrx/store';
 import { Subscription, Observable } from 'rxjs';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, FormArray, Form, AbstractControl } from '@angular/forms';
 import { RawClientDto } from './../../shared/models/client';
 import { ClientDto } from 'src/app/shared/models/client';
 import selectors from 'src/app/store/selectors';
 import { tap, startWith, map, flatMap } from 'rxjs/operators';
 import { ProductDto } from 'src/app/shared/models/product';
+import { MatAutocompleteSelectedEvent } from '@angular/material';
 
 @Component({
   selector: 'app-bill-form',
@@ -68,22 +69,54 @@ export class BillFormComponent implements OnInit, OnDestroy {
     this.billForm = new FormGroup({
       description: new FormControl(null),
       comment: new FormControl(null),
-      client: new FormControl(this.selectedClient)
+      client: new FormControl(this.selectedClient),
+      billItems: new FormArray([])
     });
 
   }
 
   onSubmit(): void {
-    delete this.billForm.value.client.bills;
-    console.log(this.billForm.value);
+    if (this.billForm.value.client.hasOwnProperty('bills')) {
+      delete this.billForm.value.client.bills;
+    }
   }
 
   displayName(product?: ProductDto): string | undefined {
     return product ? product.name : undefined;
   }
 
+  selectProduct(event: MatAutocompleteSelectedEvent): void {
+
+    const product: ProductDto = event.option.value;
+
+    const billItemControl =
+      this.getControls('billItems').find((control: FormGroup) => control.get(['product']).value.id === product.id);
+
+    if (billItemControl) {
+      (billItemControl as FormGroup).get('quantity').setValue((billItemControl as FormGroup).get('quantity').value + 1);
+    } else {
+      this.getFormArray('billItems').push(new FormGroup({
+        quantity: new FormControl(1),
+        product: new FormControl(product)
+      }));
+    }
+
+    this.autocompleteControl.setValue('');
+    event.option.focus();
+    event.option.deselect();
+
+  }
+
   private getProducts(): Observable<ProductDto[]> {
     return this.store.select(selectors.getProducts);
+  }
+
+  private getFormArray(path: (string | number)[] | string): FormArray {
+    return this.billForm.get(path) as FormArray;
+  }
+
+  getControls(path: (string | number)[] | string): AbstractControl[] {
+    return this.getFormArray(path).controls;
   }
 
   ngOnDestroy() {
