133b6970e4258484de4d222b8aa132ef938d1156
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-02-09 18:11:47 -0700

    Production server url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://localhost:8080',
+  baseUrl: 'https://spring-clients-app.herokuapp.com',
   clientId: 'angular-app',
   clientSecret: 'my-client-secret'
 };
